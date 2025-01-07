c80e906aa91d8d86fc54b7367d078be0481332ad
gervasio.dombo <gervasio.dombo@intersite.com.br>
2025-01-07 02:44:29 -0300

    feat: adding entities auditing

--- src/main/java/com/itelectric/backend/domain/entity/User.java
+++ src/main/java/com/itelectric/backend/domain/entity/User.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "t_users")
-public class User implements Serializable {
+public class User extends AbstractAuditingEntity implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
