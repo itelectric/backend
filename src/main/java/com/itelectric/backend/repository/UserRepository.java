1f120d7f4e60248d980bf1d35323b27d4ff6750c
gervasio.dombo <gervasio.dombo@intersite.com.br>
2025-01-11 23:28:43 -0300

    feat: adding find by username and by nuit in UserRepository

--- src/main/java/com/itelectric/backend/repository/UserRepository.java
+++ src/main/java/com/itelectric/backend/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.itelectric.backend.repository;
 
 import com.itelectric.backend.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,7 +11,9 @@ import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-    Optional<User> findByNuit(String nuit);
+    @Query("SELECT u FROM User u WHERE u.nuit = :nuit AND u.isDeleted = false")
+    Optional<User> findByNuit(@Param("nuit") String nuit);
 
-    Optional<User> findByUsername(String username);
+    @Query("SELECT u FROM User u WHERE u.username = :username AND u.isDeleted = false")
+    Optional<User> findByUsername(@Param("username") String username);
 }
