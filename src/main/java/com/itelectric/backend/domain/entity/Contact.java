42a9d79eeab3ca59d52db3791e2e3aa051a69488
gervasio.dombo <gervasio.dombo@intersite.com.br>
2025-01-11 23:26:02 -0300

    refactor: remove unused code

--- src/main/java/com/itelectric/backend/domain/entity/Contact.java
+++ src/main/java/com/itelectric/backend/domain/entity/Contact.java
@@ -3,6 +3,7 @@ package com.itelectric.backend.domain.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.UUID;
 
@@ -14,10 +15,10 @@ import java.util.UUID;
 @AllArgsConstructor
 @Table(name = "t_contacts")
 public class Contact implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
-    @Column(length = 32)
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
     private String phone;
