dcab820f394bf45815192d6071ae61779adc31ef
gervasio.dombo <gervasio.dombo@intersite.com.br>
2025-01-11 23:23:15 -0300

    feat: adding golbal exception handler

--- src/main/java/com/itelectric/backend/config/GlobalExceptionHandler.java
+++ src/main/java/com/itelectric/backend/config/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.itelectric.backend.config;
 
-import com.itelectric.backend.api.dto.Response;
+import com.itelectric.backend.api.v1.dto.Response;
+import com.itelectric.backend.domain.exception.BusinessException;
+import com.itelectric.backend.domain.exception.ConflictException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,5 +23,30 @@ public class GlobalExceptionHandler {
                 HttpStatus.BAD_REQUEST.name(),
                 errorMessage);
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<Response> handleBusinessExceptions(Exception ex) {
+        Response response = new Response(HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.name(),
+                ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<Response> handleConflictExceptions(Exception ex) {
+        Response response = new Response(HttpStatus.CONFLICT.value(),
+                HttpStatus.CONFLICT.name(),
+                ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Response> handleOtherExceptions(Exception ex) {
+        log.error(ex.getMessage(), ex);
+        Response response = new Response(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                HttpStatus.INTERNAL_SERVER_ERROR.name(),
+                "An unexpected error occurred. Please try again later.");
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
